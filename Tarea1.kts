144e6c235fc177eefd47099dd04a8f26c20f1569
javieriq <javier.iniguez2498@gmail.com>
2020-02-27 20:41:39 -0600

    Tarea agregado 2 ejemplos

--- Tarea1.kts
+++ Tarea1.kts
@@ -27,6 +27,8 @@ import java.util.*
 
 // P08 (*) Eliminate consecutive duplicates of list elements.
 println("Ejercicio 8")
+// val a = arrayOf("a","a","a","a","b","c","c","a","a","d","e","e","e","e")
+// val a = arrayOf("a","a","a","a","e","e","e")
 val a = arrayOf("a", "b", "c", "c", "a","a","b","b")
 val b = a.distinct()
 println(b)
@@ -43,12 +45,19 @@ fun primeN(n: Int): String {
 }
 
 println("17 " + primeN(17))
+println("7 " + primeN(7))
+println("17 " + primeN(17))
+
+
 
 //Resultado:
 //Ejercicio 8
-//[a, b, c]
+//  [a, b, c]
+//  [a,b,c,d,e]
+//  [a,e]
 //Ejercicio 31
 //17 es primo
-//
+//7 es primo
+// 4 no es primo
 //Process finished with exit code 0
 
